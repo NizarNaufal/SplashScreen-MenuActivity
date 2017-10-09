9964380519d607c5f8596221396b89cf89dc2bbb
NizarNaufal <nizarnfl21@gmail.com>
2017-10-09 18:32:16 +0700

    versi 1.0

---
+++ app/src/main/java/com/nizar/seputarrider/MenuActivity.java
@@ -0,0 +1,43 @@
+package com.nizar.seputarrider;
+
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.ViewGroup;
+
+import com.imangazaliev.circlemenu.CircleMenu;
+import com.imangazaliev.circlemenu.CircleMenuButton;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ViewGroup snackbarContainer = (ViewGroup) findViewById(R.id.snackbar_contaner);
+        CircleMenu circleMenu = (CircleMenu) findViewById(R.id.circle_menu);
+        circleMenu.setOnItemClickListener(new CircleMenu.OnItemClickListener() {
+            @Override
+            public void onItemClick(CircleMenuButton menuButton) {
+                Snackbar.make(snackbarContainer, menuButton.getHintText(), Snackbar.LENGTH_LONG).show();
+
+            }
+        });
+
+        circleMenu.setStateUpdateListener(new CircleMenu.OnStateUpdateListener() {
+            @Override
+            public void onMenuExpanded() {
+                Log.d("CircleMenuStatus", "Expanded");
+
+            }
+
+            @Override
+            public void onMenuCollapsed() {
+                Log.d("CircleMenuStatus", "Collapsed");
+            }
+        });
+    }
+}
+
